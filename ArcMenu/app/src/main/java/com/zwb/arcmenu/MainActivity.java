dfcd5a115861e59e2abd6b7c35dee6d05d8ec97f
周伟斌 <zwbfight@163.com>
2017-05-23 15:29:35 +0800

    添加图片效果

--- ArcMenu/app/src/main/java/com/zwb/arcmenu/MainActivity.java
+++ ArcMenu/app/src/main/java/com/zwb/arcmenu/MainActivity.java
@@ -2,7 +2,6 @@ package com.zwb.arcmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
